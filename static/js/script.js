58d06c9f89862284c85ba85bc9dc98519db11842
Mark O'Beirne <markobeirne00@gmail.com>
2021-01-12 16:35:11 +0000

    Break mobile classlist updates into new function

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,17 @@
 const navOptions = document.querySelectorAll(".nav-link")
 
 function setActiveNav() {
-    console.log(this);
     navOptions.forEach(option => option.classList.remove("active"));
     this.classList.add("active");
+    toggleMobileNav()
+}
+
+function toggleMobileNav() {
     const navBarTogglerBtn = document.querySelector(".navbar-toggler");
     navBarTogglerBtn.classList.add("collapsed");
     navBarTogglerBtn.setAttribute("aria-expanded", "false")
+    navBarTogglerBtn.classList.add("collapsed");
+    
     const navbarCollapse = document.querySelector("#navbarToggler")
     navbarCollapse.classList.remove("show");
 }
